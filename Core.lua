3709c6fa041afe6f2ce44c851c9cd6679e98c518
David Kosorin <david@kosorin.net>
2020-05-03 12:25:46 +0200

    Split Addon.lua into Addon.lua and Core.lua

---
+++ Core.lua
@@ -0,0 +1,50 @@
+local KOMAND, Core = ...
+_G.Komand = Core
+
+function Core.Execute(command)
+    local editBox = DEFAULT_CHAT_FRAME.editBox
+    editBox:SetText(command)
+    ChatEdit_SendText(editBox)
+end
+
+function Core.GroupComparer(a, b)
+    local aa, bb
+
+    aa = a.name:upper()
+    bb = b.name:upper()
+    if aa ~= bb then
+        return aa < bb
+    end
+
+    return aa < bb
+end
+
+function Core.ItemComparer(a, b)
+    local aa, bb
+
+    bb = (a ~= nil and a.pinned and 1 or 0)
+    aa = (b ~= nil and b.pinned and 1 or 0)
+    if aa ~= bb then
+        return aa < bb
+    end
+
+    aa = a.name:upper()
+    bb = b.name:upper()
+    if aa ~= bb then
+        return aa < bb
+    end
+
+    return aa < bb
+end
+
+function Core.ItemNameComparer(a, b)
+    local aa, bb
+
+    aa = a.name:upper()
+    bb = b.name:upper()
+    if aa ~= bb then
+        return aa < bb
+    end
+
+    return aa < bb
+end
