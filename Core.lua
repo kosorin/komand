1d8f1864b09afa1c670b19b5ba390a9728187165
David Kosorin <david@kosorin.net>
2020-05-03 21:53:25 +0200

    Add function for printing

--- Core.lua
+++ Core.lua
@@ -1,12 +1,22 @@
 local KOMAND, Core = ...
 _G.Komand = Core
 
+local AceConsole = LibStub("AceConsole-3.0")
+
 function Core.Execute(command)
     Core.Menu:Hide()
 
     local editBox = DEFAULT_CHAT_FRAME.editBox
     editBox:SetText(command)
     ChatEdit_SendText(editBox)
+end
+
+function Core.Print(...)
+    AceConsole.Print(KOMAND, ...)
+end
+
+function Core.Debug(...)
+    AceConsole.Print(KOMAND, "|cffa0a0a0", ...)
 end
 
 function Core.GroupComparer(a, b)
